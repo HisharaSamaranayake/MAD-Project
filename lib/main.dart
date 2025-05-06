6522b3d46e5dafd00657f13fec0eaca36cedbe93
LDRuwanthika <135571877+LDRuwanthika@users.noreply.github.com>
2025-05-06 10:58:28 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -77,3 +77,47 @@ class LoginPage extends StatelessWidget {
     );
   }
 }
+/*
+import 'package:flutter/material.dart';
+import 'welcome_screen.dart';
+import 'login_screen.dart';
+import 'register_screen.dart';
+import 'home_screen.dart';
+import 'nature_wildlife_page.dart';
+import 'FoodCategoryScreen.dart';
+import 'travelnote.dart';// Import FoodCategoryScreen
+import 'Emergency.dart';
+import 'my_profile.dart';
+import 'setting.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Travel App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => WelcomeScreen(),
+        '/login': (context) => LoginScreen(),
+        '/register': (context) => RegisterScreen(),
+        '/nature': (context) => NatureWildlifePage(),
+        '/home': (context) => HomeScreen(),
+        '/food': (context) => FoodCategoryScreen(),
+        '/travelnote': (context) => TravelNoteScreen(),
+        '/profile': (context) => MyProfilePage(),
+        '/emergency': (context) => EmergencyScreen(),
+        '/setting': (context) => SettingsPage(),
+
+// Ensure this route is here
+      },
+    );
+  }
+}
+ */
