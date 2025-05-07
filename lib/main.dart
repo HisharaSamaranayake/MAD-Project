48294914234bf2793f397fb8c3307132a45ba285
dnawarathna <dasuni93420@gmail.com>
2025-05-07 22:50:14 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,47 @@
 import 'package:flutter/material.dart';
-import 'splash_screen.dart';
+import 'welcome_screen.dart';
 import 'login_screen.dart';
 import 'register_screen.dart';
-import 'welcome_screen.dart'; // Import WelcomeScreen
 import 'home_screen.dart';
 import 'FoodCategoryScreen.dart';
+//import 'travelnote.dart';
 import 'Emergency.dart';
 import 'my_profile.dart';
 import 'setting.dart';
+import 'cultural_historical_page.dart';
+import 'hillcountry_scenic_page.dart'; import 'coste_beaches_page.dart';import 'nature_wildlife_page.dart';
+
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Travel App',
       theme: ThemeData(primarySwatch: Colors.blue),
-      initialRoute: '/',  // SplashScreen is shown on app launch
+      initialRoute: '/',
       routes: {
-        '/': (context) => SplashScreen(),  // Load SplashScreen first
-        '/login': (context) => LoginScreen(),  // Login screen route
-        '/register': (context) => RegisterScreen(),  // Register screen route
-        '/welcome': (context) => WelcomeScreen(),  // Welcome screen route
-        '/home': (context) => HomeScreen(),  // Home screen route after successful login
+        '/': (context) => WelcomeScreen(),
+        '/login': (context) => LoginScreen(),
+        '/register': (context) => RegisterScreen(),
+        //'/nature': (context) => NatureWildlifePage(),
+        '/home': (context) => HomeScreen(),
         '/food': (context) => FoodCategoryScreen(),
+       //'/travelnote': (context) => TravelNoteScreen(),
         '/profile': (context) => MyProfilePage(),
         '/emergency': (context) => EmergencyScreen(),
         '/setting': (context) => SettingsPage(),
+        '/nature': (context) => NatureWildlifePage(),
+        '/beach': (context) => CoastalBeachesPage(),
+        '/culture': (context) => CulturalHistoricalPage(),
+        '/hillcountry': (context) => HillCountryScenicPage(),
+
+// Ensure this route is here
       },
     );
   }
 }
-
