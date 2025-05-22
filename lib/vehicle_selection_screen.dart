faac745287c47c3cd17ffe0cab91db2f18031159
ydksoysa <135942072+ydksoysa@users.noreply.github.com>
2025-05-22 20:17:49 +0530

    Create vehicle_selection_screen.dart
    vehicle selection screen

---
+++ lib/vehicle_selection_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class VehicleSelectionScreen extends StatelessWidget {
+  final List<Map<String, dynamic>> vehicles = [
+    {
+      'name': 'Bike',
+      'price': 'LKR 200',
+      'icon': Icons.pedal_bike,
+    },
+    {
+      'name': 'Car',
+      'price': 'LKR 450',
+      'icon': Icons.directions_car,
+    },
+    {
+      'name': 'Tuk Tuk',
+      'price': 'LKR 300',
+      'icon': Icons.electric_rickshaw,
+    },
+    {
+      'name': 'Van',
+      'price': 'LKR 600',
+      'icon': Icons.airport_shuttle,
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFE6F7F2),
+      appBar: AppBar(
+        title: const Text('Select a Vehicle'),
+        backgroundColor: const Color(0xFF317873),
+      ),
+      body: ListView.builder(
+        itemCount: vehicles.length,
+        itemBuilder: (context, index) {
+          final vehicle = vehicles[index];
+          return Card(
+            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            color: const Color(0xFFCCF3D6),
+            child: ListTile(
+              leading: Icon(
+                vehicle['icon'],
+                size: 32,
+                color: const Color(0xFF317873),
+              ),
+              title: Text(
+                vehicle['name'],
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(
+                vehicle['price'],
+                style: const TextStyle(color: Colors.black54),
+              ),
+              onTap: () {
+                Navigator.pushNamed(
+                  context,
+                  '/confirm',
+                  arguments: vehicle['name'],
+                );
+              },
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
